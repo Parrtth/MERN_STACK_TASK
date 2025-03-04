fb13d33839a5c34042efb6bf2b481b5250e81cf0
Parth Patel <157614285+Parrtth@users.noreply.github.com>
2025-03-04 10:39:00 +0530

    Add files via upload

---
+++ 002.js
@@ -0,0 +1,11 @@
+let myString = "Hello, JavaScript!"; // String
+let myNumber = 42; // Number
+let myBoolean = true; // Boolean
+let myNull = null; // Null
+let myUndefined; // Undefined
+
+console.log("myString:", typeof myString); // string
+console.log("myNumber:", typeof myNumber); // number
+console.log("myBoolean:", typeof myBoolean); // boolean
+console.log("myNull:", typeof myNull); // object (JavaScript quirk)
+console.log("myUndefined:", typeof myUndefined); // undefined
