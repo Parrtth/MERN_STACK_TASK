361ab2b7fe905916408b2de1733e6c0d53e4fd4c
Parth Patel <157614285+Parrtth@users.noreply.github.com>
2025-03-11 09:17:46 +0530

    Add files via upload

---
+++ 012.js
@@ -0,0 +1,14 @@
+const person = {
+    name: "John",
+    age: 25,
+    city: "New York"
+  };
+  
+  // Updating age
+  person.age = 30;
+  
+  // Adding new property email
+  person.email = "john@example.com";
+  
+  console.log(person);
+  
