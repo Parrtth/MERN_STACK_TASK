b28e4f2ec691efb0bdc98708b3c0a5814f74076c
Parth Patel <157614285+Parrtth@users.noreply.github.com>
2025-05-26 20:06:41 +0530

    Add files via upload

---
+++ a017.js
@@ -0,0 +1,9 @@
+let numbers = [10, 20, 30, 40, 50];
+
+// Remove the last element
+numbers.pop();
+
+// Add 60 at the beginning
+numbers.unshift(60);
+
+console.log(numbers); // Output: [60, 10, 20, 30, 40]
