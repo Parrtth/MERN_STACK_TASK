cf6dc9df7e016eca6c68b25628a00a8704a88a84
Parth Patel <157614285+Parrtth@users.noreply.github.com>
2025-05-29 01:28:55 +0530

    Add files via upload

---
+++ 009.js
@@ -0,0 +1,13 @@
+let person = {
+    name: "Alice",
+    age: 25,
+    city: "New York"
+};
+
+// Adding a new property
+person.country = "USA";
+
+// Updating the age
+person.age = 23;
+
+console.log(person);
