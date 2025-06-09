a8cab77a129c4e217e9b0632cd3e3613188fbd68
Parth Patel <157614285+Parrtth@users.noreply.github.com>
2025-06-09 11:09:56 +0530

    Add files via upload

---
+++ 008.js
@@ -0,0 +1,9 @@
+let person = {
+    name: "Alice",
+    age: 25,
+    city: "New York"
+};
+
+console.log("Name:", person.name);
+console.log("Age:", person.age);
+console.log("City:", person.city);
